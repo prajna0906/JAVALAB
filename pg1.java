e2ffa5a941beeb1780bc25a9a3052fec28eb3890
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-11-15 19:12:37 +0530

    Update pg1.java

--- pg1.java
+++ pg1.java
@@ -1,31 +1,29 @@
 import java.util.*;
-
-class prime {
- public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in); {
-        System.out.println("enter the range");
-        int n = sc.nextInt();
-        int a[]=new int[n];
-        for(int i=1; i<=n; i++)
-        {
-            a[i-1]=i;
-        }
-        for (int i=1; i<n; i++)
-        {
-            if(a[1]!=0)
-        { for(int j=i+a[i]; j<n; j+=a[i])
-        {
-            a[j]=0;
-        } }
-        
-}
-for (int i=0; i<n; i++)
+class prime
 {
-        if (a[i]!=0)
-        {
-            System.out.println(a[i]+"\t");
-        }
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+  System.out.println("Enter the range");
+  int n=sc.nextInt();
+  int a[]=new int[n];
+  for(int i=1;i<=n;i++)
+   {
+     a[i-1]=i;
+   }
+  for(int i=1;i<n;i++)
+    {  if(a[i]!=0)
+      {for(int j=i+a[i];j<n;j+=a[i])
+       { 
+         a[j]=0;
+       }}
+    }
+  for(int i=0;i<n;i++)
+    {
+      if(a[i]!=0)
+      {
+        System.out.print(a[i]+"\t");
 }
- }
- }
+}
+}
 }
